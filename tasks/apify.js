1c5b5c4fbfe74e19813ef0b03e5cccf985132f50
Chema Balsas <jbalsas@gmail.com>
2013-11-13 21:19:05 +0100

    Properly resolve templates path

--- tasks/apify.js
+++ tasks/apify.js
@@ -13,16 +13,17 @@ module.exports = function (grunt) {
     grunt.registerMultiTask("apify", "Grunt plugin for apify", function () {
         var defaults    = require("apify/lib/defaults.json"),
             documenter  = require("apify/lib/Documenter"),
+            path        = require("path"),
             done        = this.async();
         
         // Resolve absolute path for default templates and assets
         var templates = Object.keys(defaults.templates);
         
         templates.forEach(function (templateName) {
-            defaults.templates[templateName] = "node_modules/apify/bin/" + defaults.templates[templateName];
+            defaults.templates[templateName] = path.resolve(__dirname + "/../node_modules/apify/bin/" + defaults.templates[templateName]);
         });
         
-        defaults.assets = "node_modules/apify/bin/" + defaults.assets;
+        defaults.assets = path.resolve(__dirname + "/../node_modules/apify/bin/" + defaults.assets);
         
         defaults.source = this.src || "src";
         defaults.output = this.output || "docs";
